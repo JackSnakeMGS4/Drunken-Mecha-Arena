ef722e9bf5deb1a4d751a5b75ed967361fbab8a9
Jaime Rivas <jaimerivas81@outlook.com>
2019-05-15 08:27:16 -0700

    flight control accelerates, deccelerates, and accounts for rising/falling; camera zooms out as player moves faster and closes in for the lower speeds; still has bugs but it works for a prototype

--- Assets/Scripts/Player/SimpleFlight.cs
+++ Assets/Scripts/Player/SimpleFlight.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class SimpleFlight : MonoBehaviour
 {
-    [SerializeField] private float vel = 0.0f;
+    private float vel = 0.0f;
     [SerializeField] private float thrust = 20.0f;
+    [SerializeField] private float maxVel = 200.0f;
     private Transform t;
+    public GameObject ground;
 
     void Awake()
     {
@@ -15,20 +17,57 @@ public class SimpleFlight : MonoBehaviour
 
     void Update()
     {
-        AccelarateOrDeccelerate();
+        MovePlayer();
+        ContainPlayerToArea();
     }
 
-    void AccelarateOrDeccelerate()
+    void MovePlayer()
     {
-        if(Input.GetButton("Thrust"))
+        if(Input.GetButton("Thrust") && vel < maxVel)
         {
             vel += thrust;
         }
-        else if(Input.GetButton("De-Throttle"))
+        else if(Input.GetButton("De-Throttle") && vel > (0.05f * maxVel))
         {
             vel *= 0.97f;
         }
         
-        t.position -= t.forward * Time.deltaTime * vel;
+        if(vel <= maxVel)
+        {
+            t.position += -(t.forward * Time.deltaTime * vel);
+            vel -= -(transform.forward.y * Time.deltaTime * 30.0f);
+        }
+        else
+        {
+            vel = maxVel;
+        }
+        
+        transform.Rotate( -Input.GetAxis("Vertical"), 0.0f, Input.GetAxis("Horizontal"));
+    }
+
+    void ContainPlayerToArea()
+    {
+        if(ground.transform.position.y + 0.5f > t.position.y)
+        {
+            t.position = new Vector3 (t.position.x, ground.transform.position.y + 0.5f, t.position.z);
+        }
+
+        if(ground.transform.position.x + 1000.0f < t.position.x)
+        {
+            t.position = new Vector3 (ground.transform.position.x - 1000.0f,t.position.y,t.position.z);
+        }
+        else if(ground.transform.position.x - 1000.0f > t.position.x)
+        {
+            t.position = new Vector3 (ground.transform.position.x + 1000.0f,t.position.y,t.position.z);
+        }
+
+        if(ground.transform.position.z + 1000.0f < t.position.z)
+        {
+            t.position = new Vector3 (t.position.x,t.position.y,ground.transform.position.z - 1000.0f);
+        }
+        else if(ground.transform.position.z - 1000.0f > t.position.z)
+        {
+            t.position = new Vector3 (t.position.x,t.position.y,ground.transform.position.z + 1000.0f);
+        }
     }
 }
