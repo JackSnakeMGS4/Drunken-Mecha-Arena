903f0774bf41ca74b967ef3f6aa6f08743cbaa38
Jaime Rivas <jaimerivas81@outlook.com>
2019-05-30 09:45:47 -0700

    Prototype mecha halfway done; Included Cinemachine package; added a simple cursor (player firing line and reticle still not lined up)

--- Assets/Scripts/Gameplay/AmmoAndShooting.cs
+++ Assets/Scripts/Gameplay/AmmoAndShooting.cs
@@ -6,10 +6,12 @@ public class AmmoAndShooting : MonoBehaviour
 {
     [SerializeField] private GameObject ammo1;
     [SerializeField] private GameObject ammo2;
+
+    [SerializeField] private Canvas canvas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
