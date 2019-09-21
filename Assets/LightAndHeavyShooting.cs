0867e0c55840aece8411d277309150b348d0f6e7
Jaime Rivas <jaimerivas81@outlook.com>
2019-09-21 02:21:47 -0700

    Muzzle flash particle play while firing

--- Assets/LightAndHeavyShooting.cs
+++ Assets/LightAndHeavyShooting.cs
@@ -8,13 +8,15 @@ public class LightAndHeavyShooting : MonoBehaviour
     //TODO: Increase fireRate to 5 if heavy mode is on and fire heavy shot
     //TODO: Increase damage to 100 i heavy mode is on
     //TODO: Decrease shot range to 20 if heavy mode is on
+    //TODO: Add shot landing VFX to container and delete each one after 2 seconds
     [SerializeField] [Range(0.25f, 5.0f)] private float fireRate = 1;
     [SerializeField] [Range(1, 100)] private int damage = 1;
     [SerializeField] [Range(100.0f, 20.0f)] private float shotRange = 100.0f;
     private float timer;
 
     [SerializeField] private Transform firePoint;
-    [SerializeField] private GameObject shotLanding;
+    [SerializeField] private ParticleSystem shotImpact;
+    [SerializeField] private ParticleSystem muzzleFlash;
 
     // Update is called once per frame
     void Update()
@@ -32,18 +34,20 @@ public class LightAndHeavyShooting : MonoBehaviour
 
     private void FireWeapon()
     {
-        Debug.DrawRay(firePoint.position, firePoint.forward * shotRange, Color.red, 2.0f);
-
         Ray ray = new Ray(firePoint.position, firePoint.forward);
         RaycastHit hitInfo;
-        
-        if(Physics.Raycast(ray, out hitInfo, shotRange))
+
+        muzzleFlash.Play();
+
+        if (Physics.Raycast(ray, out hitInfo, shotRange))
         {
             var health = hitInfo.collider.GetComponent<Health>();
             if(health != null)
             {
                 health.TakeDamage(damage);
             }
+
+            Instantiate(shotImpact, hitInfo.point, Quaternion.identity);
         }
     }
 }
