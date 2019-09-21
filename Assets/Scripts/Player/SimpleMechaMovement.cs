28b6a8633885d74d7d1443d2c083efbad8beecf1
Jaime Rivas <jaimerivas81@outlook.com>
2019-09-20 18:30:42 -0700

    MagicaVoxel material added; voxel models display properly; voxel models also work with ProGrids; sample terrain and props added; sample colosseum walls added; duplicated player and renamed it to enemy

--- Assets/Scripts/Player/SimpleMechaMovement.cs
+++ Assets/Scripts/Player/SimpleMechaMovement.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class SimpleMechaMovement : MonoBehaviour
 {
-    private float vel = 0.0f;
     [SerializeField] private float speed = 1.0f;
-    [SerializeField] private float maxSpeed = 50.0f;
     private Transform t;
     public GameObject ground;
 
@@ -28,11 +26,11 @@ public class SimpleMechaMovement : MonoBehaviour
 
     void MovePlayer()
     {
-        t.Translate(Vector3.forward * speed * -Input.GetAxis("Vertical") * Time.deltaTime);
-        t.Translate(Vector3.right * speed * -Input.GetAxis("Horizontal") * Time.deltaTime);
+        t.Translate(Vector3.forward * speed * Input.GetAxis("Vertical") * Time.deltaTime);
+        t.Translate(Vector3.right * speed * Input.GetAxis("Horizontal") * Time.deltaTime);
         t.Translate(Vector3.up * speed * Input.GetAxis("Ascend/Descend") * Time.deltaTime);
 
-        transform.Rotate(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0.0f);
+        transform.Rotate(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0.0f);
 
         if (Input.GetKeyDown(KeyCode.U))
         {
@@ -42,27 +40,27 @@ public class SimpleMechaMovement : MonoBehaviour
 
     void ContainPlayerToArea()
     {
-        if (ground.transform.position.y + 0.5f > t.position.y)
+        if (ground.transform.position.y + 1.0f > t.position.y)
         {
-            t.position = new Vector3(t.position.x, ground.transform.position.y + 0.5f, t.position.z);
+            t.position = new Vector3(t.position.x, ground.transform.position.y + 1.0f, t.position.z);
         }
 
-        if (ground.transform.position.x + 1000.0f < t.position.x)
+        if (ground.transform.position.x + 180.0f < t.position.x)
         {
-            t.position = new Vector3(ground.transform.position.x - 1000.0f, t.position.y, t.position.z);
+            t.position = new Vector3(ground.transform.position.x - 180.0f, t.position.y, t.position.z);
         }
-        else if (ground.transform.position.x - 1000.0f > t.position.x)
+        else if (ground.transform.position.x - 180.0f > t.position.x)
         {
-            t.position = new Vector3(ground.transform.position.x + 1000.0f, t.position.y, t.position.z);
+            t.position = new Vector3(ground.transform.position.x + 180.0f, t.position.y, t.position.z);
         }
 
-        if (ground.transform.position.z + 1000.0f < t.position.z)
+        if (ground.transform.position.z + 180.0f < t.position.z)
         {
-            t.position = new Vector3(t.position.x, t.position.y, ground.transform.position.z - 1000.0f);
+            t.position = new Vector3(t.position.x, t.position.y, ground.transform.position.z - 180.0f);
         }
-        else if (ground.transform.position.z - 1000.0f > t.position.z)
+        else if (ground.transform.position.z - 180.0f > t.position.z)
         {
-            t.position = new Vector3(t.position.x, t.position.y, ground.transform.position.z + 1000.0f);
+            t.position = new Vector3(t.position.x, t.position.y, ground.transform.position.z + 180.0f);
         }
     }
 }
